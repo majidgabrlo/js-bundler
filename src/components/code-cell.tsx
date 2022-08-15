31a23a8c8bc869b488878ff65ea8d9d81246a3b1
Majid Gabrlö <majidgabrlo@gmail.com>
2022-08-15 17:15:38 +0330

    initial input

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import bundle from "../bundler";
+import Resizable from "./resizable";
+
+const CodeCell = () => {
+  const [code, setCode] = useState("");
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  return (
+    <Resizable direction="vertical">
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={`import React from 'react'\nimport ReactDOM from 'react-dom'\n\nReactDOM.render(<div></div>, document.querySelector('#root'));
+            `}
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
